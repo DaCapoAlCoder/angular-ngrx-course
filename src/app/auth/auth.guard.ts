62379caa88d7cb0bfc0a9efae2833900f29bc875
Peter Murray <peter.murray@FirstData.com>
2019-06-07 15:12:03 +0100

    section 2 part 18 complete

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '../reducers';
@@ -9,13 +9,16 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private store: Store<AppState>, private router: Router){ }
+    constructor(private store: Store<AppState>, private router: Router) {}
+
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         return this.store.pipe(
             select(isLoggedIn),
             tap( loggedIn => {
-                this.router.navigateByUrl('login');
+                if (!loggedIn) {
+                    this.router.navigateByUrl('login');
+                }
             })
         );
     }
