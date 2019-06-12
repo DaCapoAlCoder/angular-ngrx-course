c255445c8011610f5382ae88130f36e5b7f85696
Peter Murray <peter.murray@FirstData.com>
2019-06-12 14:29:30 +0100

    Section 4 complete

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -3,18 +3,21 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
 } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../environments/environment';
 import { User } from '../model/user.model';
 import { AuthActionTypes } from '../auth/auth.actions';
+import { routerReducer } from '@ngrx/router-store';
 
 
 export interface AppState {
 }
 
 export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
